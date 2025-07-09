f563cea5bc7b3d35f6c577ecf02650257f2308a7
MarleneMS09 <marlenemsanchez05@gmail.com>
2025-07-08 22:01:21 -0600

    Add styles to cursor - 00

--- js/main.js
+++ js/main.js
@@ -248,3 +248,9 @@ navLinks.forEach(link => {
 
 
 
+const cursor = document.querySelector(".custom-cursor");
+
+  window.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+  });
